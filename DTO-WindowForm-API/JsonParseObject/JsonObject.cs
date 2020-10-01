2f9085742f9c6d42e7130c0f2f3b0e0fbc59d541
Lam B Nguyen <l0nguy10@louisville.edu>
2020-10-01 16:19:29 -0400

    DTO for API WIndow Form

--- DTO-WindowForm-API/JsonParseObject/JsonObject.cs
+++ DTO-WindowForm-API/JsonParseObject/JsonObject.cs
@@ -20,34 +20,38 @@ namespace DTO_WindowForm_API.JsonParseObject
 {//https://www.c-sharpcorner.com/UploadFile/raj1979/consume-Asp-Net-web-api-using-httpclient-in-wpf/
     public class JsonObject
     {
-        public List<Person> Get()
+        HttpClient client;
+          public JsonObject()
         {
-            HttpClient client = new HttpClient();
+            client = new HttpClient();
             client.BaseAddress = new Uri("https://beautysignindev.azurewebsites.net/signin");
+
+            client.DefaultRequestHeaders.Accept.Add(
+                   new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public List<Person> Get()//Get data from the API
+        {   //using Http Client to make the get request 
+           
             //client.DefaultRequestHeaders.Add("appkey", "myapp_key");
+            //Tell Server that we want the JSON format
             client.DefaultRequestHeaders.Accept.Add(
                new MediaTypeWithQualityHeaderValue("application/json"));
-
+            //Get the response after send the request
              HttpResponseMessage response = client.GetAsync("").Result;
-           
-                var employees = response.Content.ReadAsAsync<List<Person>>().Result;
-                return employees;
+           //Save the Response content in the persons variable
+                var persons = response.Content.ReadAsAsync<List<Person>>().Result;
+                return persons;
             
            
         }
+        //Make Post request 
         public HttpResponseMessage Post(string FirstName, string LastName,string Phone)
-        {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://beautysignindev.azurewebsites.net/signin");
-            //client.DefaultRequestHeaders.Add("appkey", "myapp_key");
-            client.DefaultRequestHeaders.Accept.Add(
-               new MediaTypeWithQualityHeaderValue("application/json"));
-
+        {   
             var person = new Person();
             person.firstName = FirstName;
             person.lastName = LastName;
             person.phone = Phone;
-
+            //Save the response after execute the post request
             var response = client.PostAsJsonAsync("", person).Result;
             return response;
 
