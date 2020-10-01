6f820d7b582c4205d30ca4aea6837fe6dfabde0e
Lam B Nguyen <l0nguy10@louisville.edu>
2020-10-01 15:28:09 -0400

    Update DTO for SQL Server Compact

---
+++ DTO-SQLServerCompact/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_SQLServerCompact
+{
+    public class Person
+    {   public Person(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
